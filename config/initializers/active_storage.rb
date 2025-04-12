7e2f68573e0ee7e39b45d12eb664018639147352
heidless <lockhart.r@gmail.com>
2025-04-12 10:19:00 +0100

    active_storage tuning

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -9,10 +9,24 @@ Rails.configuration.to_prepare do
       else
         self.class.generate_unique_secure_token
       end
+      puts "#############################################"
+      puts "self.key :: #{self.key}"
+      puts "#############################################"
     end
 
     def prefix
-      Rails.application.credentials.dig(:gcs_storage, :gcs_storage_prefix)
+      if Rails.env.production?
+        return nil
+      else
+        storage_env = "-DEV"        
+      end
+
+      puts "#####################################"
+      puts " TEST LOG"
+      puts "#{Rails.application.credentials.dig(:gcs_storage, :gcs_storage_prefix)}#{storage_env}"
+      puts "#####################################"
+
+      "#{Rails.application.credentials.dig(:gcs_storage, :gcs_storage_prefix)}#{storage_env}"
     end
   end
 end
